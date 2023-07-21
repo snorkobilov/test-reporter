099723fbf22354e44ed19e68fad147966b6292aa
Sadriddin Norkobilov <sadriddin.norkobilov@cvshealth.com>
2023-07-21 15:23:24 -0500

    removed commented out code

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -15,8 +15,5 @@ export async function listFiles(): Promise<string[]> {
 }
 
 function fixStdOutNullTermination(): void {
-  // Previous command uses NULL as delimiters and output is printed to stdout.
-  // We have to make sure next thing written to stdout will start on new line.
-  // Otherwise things like ::set-output wouldn't work.
   core.info('')
 }
